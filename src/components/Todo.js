fe6cce1392619e14ff9e7d4ce4d25f922470ab17
masontrang <mt9260@gmail.com>
2022-04-21 23:00:27 -0400

    commit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,35 +1,37 @@
-import { useState } from 'react';
-import Modal from './Modal';
-import Backdrop from './Backdrop';
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
+// function Todo({text, other}) {   // same as big fat props object props.text , props.other - destructuring
 
 function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-
-    function deleteHandler() {
-        // console.log("clicked");
-        // console.log(props.text); 
-        setModalIsOpen(true);
-    }
-
-    function closeModalHandler() {
-        setModalIsOpen(false);
-    }
-
-
-    return(
-    <div className="card">
-          <h2>{props.text}</h2>
-          <div className='actions'>
-            {/* <span>A Span</span> */}
-            <button className='btn' onClick={deleteHandler} >Delete</button>
-          </div>
-          {modalIsOpen && (
-            <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
-          )}
-          {modalIsOpen && <Backdrop onCancel={closeModalHandler}/>}
-        </div>
-    )
+  function deleteHandler() {
+    // console.log("clicked");
+    // console.log(props.text);
+    setModalIsOpen(true);
   }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
+  return (
+    <div className="card">
+      <h2>{props.text}</h2>
+      <div className="actions">
+        {/* <span>A Span</span> */}
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
+      </div>
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
+    </div>
+  );
+}
 
 export default Todo;
